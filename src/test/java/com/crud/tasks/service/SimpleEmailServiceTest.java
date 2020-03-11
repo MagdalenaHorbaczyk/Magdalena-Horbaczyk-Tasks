739645f4812503c8c39f6b326690a37c71a64485
Magdalena <magdalena.horbaczyk@gmail.com>
2020-03-11 16:12:26 +0100

    23.3

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,41 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmailWithCc() throws Exception {
+        //given
+        Mail mail = new Mail("test@test.com", "Test", "Test message", "test1@test1.com");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        mailMessage.setCc(mail.getToCc());
+
+
+        //when
+        simpleEmailService.send(mail);
+
+        //then
+        verify(javaMailSender, times(1)).send(mailMessage);
+
+    }
+}
